6f62405b2f79abbc5cf0fc0ee57ed3202a25215d
Rob Godfrey <rob.godfrey@gmail.com>
2023-09-26 17:50:55 +0100

    Update get_temporary_credentials.sh
    Add checks for environment variables

--- src/scripts/get_temporary_credentials.sh
+++ src/scripts/get_temporary_credentials.sh
@@ -3,7 +3,22 @@
 # shellcheck disable=SC2016
 # shellcheck disable=SC2129
 
-echo "Getting Cloudsmith OIDC token..."
+if [ -z "$CLOUDSMITH_SERVICE" ]
+then
+  echo "Unable to generate OIDC token. Environment variable CLOUDSMITH_SERVICE is not set."
+  exit 1
+fi
+
+if [ -z "$CLOUDSMITH_REPOSITORY" ]
+then
+  echo "Unable to generate OIDC token. Environment variable CLOUDSMITH_REPOSITORY is not set."
+  exit 1
+fi
+
+echo "Getting Cloudsmith OIDC token for: "
+echo "CLOUDSMITH_SERVICE:     $CLOUDSMITH_SERVICE"
+echo "CLOUDSMITH_REPOSITORY:  $CLOUDSMITH_REPOSITORY"
+echo "..."
 
 RESPONSE=$(curl -X POST -H "Content-Type: application/json" -d "{\"oidc_token\":\"$CIRCLE_OIDC_TOKEN_V2\", \"service_slug\":\"$CLOUDSMITH_SERVICE\"}" "https://api.cloudsmith.io/openid/financial-times/")
 
@@ -11,12 +26,12 @@ CLOUDSMITH_OIDC_TOKEN=$(echo "$RESPONSE" | grep -o '"token": "[^"]*' | grep -o '
 
 if [ -z "$CLOUDSMITH_OIDC_TOKEN" ]
 then
-  echo "Unable to generate OIDC token"
+  echo "Unable to generate OIDC token."
+  echo "Response from Cloudsmith OIDC endpoint was: "
   echo "$RESPONSE"
   exit 1
 else
-  echo "Successfully generated OIDC token"
-  echo 'Please reference the token as $CLOUDSMITH_OIDC_TOKEN'
+  echo "Successfully generated OIDC token."
 
   CLOUDSMITH_PYTHON_REPOSITORY_URL="https://packages.ft.com/basic/$CLOUDSMITH_REPOSITORY/python/simple/"
   CLOUDSMITH_PIP_INDEX_URL="https://$CLOUDSMITH_SERVICE:$CLOUDSMITH_OIDC_TOKEN@packages.ft.com/basic/$CLOUDSMITH_REPOSITORY/python/simple/"
