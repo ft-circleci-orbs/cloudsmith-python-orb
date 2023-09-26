90ede8e0d4cc1c8925ad5caae2bb5b2ddf7bb86c
Seraphina Anderson <seraphina.anderson@gmail.com>
2023-09-26 16:54:39 +0100

    Refactor script and variables

--- src/scripts/get_temporary_credentials.sh
+++ src/scripts/get_temporary_credentials.sh
@@ -1,32 +1,25 @@
 #!/bin/bash
 
-SERVICE_SLUG="code-management"
+echo "Getting Cloudsmith OIDC token..."
 
-# Function to get a Cloudsmith API Key via OIDC
-getOidcToken() {
-  echo "Getting Cloudsmith API Key via OIDC..."
-  RESPONSE=$(curl -X POST -H "Content-Type: application/json" -d "{\"oidc_token\":\"$CIRCLE_OIDC_TOKEN_V2\", \"service_slug\":\"$SERVICE_SLUG\"}" "https://api.cloudsmith.io/openid/financial-times/")
-# Check if the response contains the "token" key
-if [[ $RESPONSE == *'"token"'* ]]; then
-  # If the "token" key is present, it's considered a success
-  API_KEY=$(echo "$RESPONSE" | grep -o '"token": *"[^"]*"' | cut -d":" -f2 | tr -d '" ')
-  echo "API key retrieved successfully"
-  echo "$API_KEY"
+RESPONSE=$(curl -X POST -H "Content-Type: application/json" -d "{\"oidc_token\":\"$CIRCLE_OIDC_TOKEN_V2\", \"service_slug\":\"$CLOUDSMITH_SERVICE\"}" "https://api.cloudsmith.io/openid/financial-times/")
+
+CLOUDSMITH_OIDC_TOKEN=$(echo "$RESPONSE" | grep -o '"token": "[^"]*' | grep -o '[^"]*$')
+
+if [ -z "$CLOUDSMITH_OIDC_TOKEN" ]
+then
+  echo "Unable to generate OIDC token"
+  echo "$RESPONSE"
+  exit 1
 else
-  # If the "token" key is not present, it's considered an error`    `
-  # Parse through response to get error
-  error=$(echo "$RESPONSE" | grep -o '"detail": *"[^"]*"' | cut -d '"' -f 4 | tr -d '\n')
-  echo "API request failed: $error"
+  echo "Successfully generated OIDC token"
+  echo 'Please reference the token as $CLOUDSMITH_OIDC_TOKEN'
+
+  CLOUDSMITH_PYTHON_REPOSITORY_URL="https://packages.ft.com/basic/$CLOUDSMITH_REPOSITORY/python/simple/"
+  CLOUDSMITH_PIP_INDEX_URL="https://$CLOUDSMITH_SERVICE:$CLOUDSMITH_OIDC_TOKEN@packages.ft.com/basic/$CLOUDSMITH_REPOSITORY/python/simple/"
+
+  echo "export CLOUDSMITH_PYTHON_REPOSITORY_URL=\"$CLOUDSMITH_PYTHON_REPOSITORY_URL\"" >> "$BASH_ENV"
+  echo "export CLOUDSMITH_PIP_INDEX_URL=\"$CLOUDSMITH_PIP_INDEX_URL\"" >> "$BASH_ENV"
+  echo "export CLOUDSMITH_OIDC_TOKEN=\"$CLOUDSMITH_OIDC_TOKEN\"" >> "$BASH_ENV"
+  echo "export CLOUDSMITH_SERVICE=\"$CLOUDSMITH_SERVICE\"" >> "$BASH_ENV"
 fi
-}
-
-
-# configureEnvironment() {
-#   # 
-# }
-
-# Retrieve Cloudsmith API key via OIDC
-getOidcToken
-
-
-# configureEnvironment()
