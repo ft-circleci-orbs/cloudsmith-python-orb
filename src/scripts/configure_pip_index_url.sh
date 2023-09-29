9cd3d8211519c07d08cf79907cf1621eb4b38455
robgodfrey <rob.godfrey@ft.com>
2023-09-29 17:56:49 +0100

    Add configure-pip command

---
+++ src/scripts/configure_pip_index_url.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# shellcheck disable=SC2016
+# shellcheck disable=SC2129
+
+set +e
+
+if [ -z "$CLOUDSMITH_PIP_INDEX_URL" ]
+then
+  echo "Unable to configure pip. Env var CLOUDSMITH_PIP_INDEX_URL is not defined."
+  exit 1
+fi
+
+python -m pip config set global.index-url $CLOUDSMITH_PIP_INDEX_URL
+
+echo "Pip index-url configured successfully for Cloudsmith repository: $CLOUDSMITH_REPOSITORY_IDENTIFIER"
