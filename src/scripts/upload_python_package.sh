a5f686aeb3ee0e41d3c91ed03ca9656a63026441
Rob Godfrey <rob.godfrey@gmail.com>
2023-10-09 17:51:44 +0100

    Extract script (#27)
    * Extract command steps into bash script
    * Convert repository parameter to env var
    * Add check for no tar.gz or whl files

---
+++ src/scripts/upload_python_package.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# shellcheck disable=SC2016
+# shellcheck disable=SC2129
+
+# Check required environment variables are set and look valid
+if [ -z "$CLOUDSMITH_ORGANISATION" ]
+then
+  echo "Unable to upload package. Env var CLOUDSMITH_ORGANISATION is not defined."
+  exit 1
+fi
+
+if [ -z "$CLOUDSMITH_REPOSITORY" ]
+then
+  echo "Unable to upload package. Env var CLOUDSMITH_REPOSITORY is not defined."
+  exit 1
+fi
+
+if [ -d "$DIST_DIR" ]; then
+  echo "$DIST_DIR is a valid directory."
+else
+  echo "$DIST_DIR is not a directory."
+  exit 1
+fi
+
+# Check there are tar.gz or whl files to upload
+LS_TAR_GZ_CMD="ls -A ${DIST_DIR}/*.tar.gz"
+LS_WHL_CMD="ls -A ${DIST_DIR}/*.whl"
+
+if [ -z "$($LS_TAR_GZ_CMD)" ] && [ -z "$($LS_WHL_CMD)" ]
+then
+  echo "$DIST_DIR does not contain any tar.gz or whl files to upload."
+  exit 1
+fi
+
+# Upload source distribution if present
+for filename in "$DIST_DIR"/*.tar.gz
+do
+  [ -f "$filename" ] || continue
+
+  echo "Uploading source distribution $filename to Cloudsmith repository $CLOUDSMITH_ORGANISATION/$CLOUDSMITH_REPOSITORY ..."
+
+  cloudsmith push python --verbose --api-key "$CLOUDSMITH_OIDC_TOKEN" "$CLOUDSMITH_ORGANISATION/$CLOUDSMITH_REPOSITORY" "$filename"
+
+  echo ""
+  echo "Package upload and synchronisation completed OK."
+done
+
+# Upload wheel distribution(s) if present
+for filename in "$DIST_DIR"/*.whl
+do
+  [ -f "$filename" ] || continue
+
+  echo "Uploading wheel $filename to Cloudsmith repository $CLOUDSMITH_ORGANISATION/$CLOUDSMITH_REPOSITORY ..."
+
+  cloudsmith push python --verbose --api-key "$CLOUDSMITH_OIDC_TOKEN" "$CLOUDSMITH_ORGANISATION/$CLOUDSMITH_REPOSITORY" "$filename"
+
+  echo ""
+  echo "Package upload and synchronisation completed OK."
+done
