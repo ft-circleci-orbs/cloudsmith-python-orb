955ca604797bc14c3b9cac30bec1d6f3f40c121e
Seraphina Anderson <seraphina.anderson@gmail.com>
2023-11-24 11:08:07 +0000

    Tweak bash script for Poetry Command

---
+++ src/scripts/set_env_vars_for_poetry.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# shellcheck disable=SC2016
+# shellcheck disable=SC2129
+
+set +e
+
+if [ -z "$CLOUDSMITH_SERVICE_ACCOUNT" ] || [ -z "$CLOUDSMITH_OIDC_TOKEN" ]
+then
+  echo "Unable to find an OIDC token to use. Please ensure the cloudsmith-oidc/authenticate_with_oidc command has been run before this command."
+  exit 1
+fi
+
+if [ -z "$CLOUDSMITH_REPOSITORY" ]
+then
+  echo "Unable to set environment variables for pip. Env var CLOUDSMITH_REPOSITORY is not defined."
+  exit 1
+fi
+
+if [ -z "$CLOUDSMITH_DOWNLOADS_DOMAIN" ]
+then
+  echo "Unable to set environment variables for pip. Env var CLOUDSMITH_DOWNLOADS_DOMAIN is not defined."
+  exit 1
+fi
+
+echo "export CLOUDSMITH_POETRY_USERNAME=\"$CLOUDSMITH_SERVICE_ACCOUNT\"" >> "$BASH_ENV"
+echo "export CLOUDSMITH_POETRY_PASSWORD=\"$CLOUDSMITH_OIDC_TOKEN\"" >> "$BASH_ENV"
+
+echo "The following environment variables have been exported. Note, the OIDC token has been masked below."
+echo ""
+echo "CLOUDSMITH_POETRY_SOURCE_URL=https://$CLOUDSMITH_DOWNLOADS_DOMAIN/basic/$CLOUDSMITH_REPOSITORY/python/simple/"
