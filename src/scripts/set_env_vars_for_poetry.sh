b6ebd55126c9cc8e5e1a1a5cc6ddce240d95196a
Seraphina Anderson <seraphina.anderson.consultant@ft.com>
2023-12-08 12:07:09 +0000

    Create poetry example (#32)
    * Create poetry examples
    
    * WIP: update poetry install package example.
    
    * Create example for installing dependencies
    
    * Create upload package example for Poetry
    
    * Add new line at end of file
    
    * Poetry is included with the image
    
    * Tweaking examples and adding tests
    
    * Add description to upload example
    
    * Add test-set_env_vars_for_poetry to jobs
    
    * Tweak poetry command
    
    * Tweak bash script for Poetry Command
    
    * Remove trailing spaces
    
    * Add space to if statement
    
    * Remove $'s
    
    * Update poetry parameters
    
    * Update repo name
    
    * Add verbose mode
    
    * test upload package example for poetry
    
    * Create simple package for twine, poetry and cli
    
    * Call poetry upload example in test
    
    * Rename test package
    
    * Revert back to simplepkg-py
    
    * Tweak poetry upload test
    
    * Fix indentation
    
    * Update poetry command
    
    * Update poetry auth params
    
    * Update toml file
    
    * Update toml file
    
    * Update poetry upload example
    
    * Update directory
    
    * check that poetry-package location
    
    * Update poetry install example
    
    * Update commands
    
    * Update poetry install example
    
    * Remove lock command
    
    * Remove version
    
    * Revert to previous approach
    
    * Add description and author
    
    * Change package version
    
    * Change order of tests and update url
    
    * Update poetry install command
    
    * Add simplepkg
    
    * Update poetry install test
    
    * Add a package to the toml file
    
    * Include package version
    
    * Try requests library
    
    * Remove lock
    
    * Try verbose mode for debugging issue
    
    * Try making library install more verbose
    
    * Remove authentication
    
    * Add authentication back in and remove poetry shell
    
    * Update commands
    
    * Remove add simplepkg
    
    * Update command naming and remove poetry add command
    
    * cd into poetry package
    
    * Refactor tests
    
    * Update examples to reflect changes
    
    * Update src/commands/set_env_vars_for_poetry.yml
    
    Co-authored-by: robertboulton <robert.boulton@ft.com>
    
    * Update src/examples/poetry_install_packages.yml
    
    Co-authored-by: robertboulton <robert.boulton@ft.com>
    
    * Update example
    
    * Update source url label and remove upload command
    
    * Remove superfluous directory block
    
    * Add comments to explain arbitrary label cloudsmith-source-url
    
    * Remove redundant packages
    
    * Move comment to the description section
    
    * Move comments to the description section
    
    * Remove white space
    
    * Add poetry dependency
    
    * Add lock argument
    
    * Add -q switch
    
    * Add poetry show
    
    * Update poetry install example
    
    * Update .circleci/test-deploy.yml
    
    Co-authored-by: robertboulton <robert.boulton@ft.com>
    
    * Update src/examples/poetry_install_packages.yml
    
    Co-authored-by: robertboulton <robert.boulton@ft.com>
    
    * Update src/examples/upload_package_using_poetry.yml
    
    Co-authored-by: robertboulton <robert.boulton@ft.com>
    
    * Update src/scripts/set_env_vars_for_poetry.sh
    
    Co-authored-by: robertboulton <robert.boulton@ft.com>
    
    * Update src/examples/poetry_install_packages.yml
    
    Co-authored-by: robertboulton <robert.boulton@ft.com>
    
    * Update src/scripts/set_env_vars_for_poetry.sh
    
    Co-authored-by: robertboulton <robert.boulton@ft.com>
    
    * Remove test blocks
    
    * Update orb version
    
    * Update simplepkg version
    
    ---------
    
    Co-authored-by: robertboulton <robert.boulton@ft.com>

---
+++ src/scripts/set_env_vars_for_poetry.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# shellcheck disable=SC2016
+# shellcheck disable=SC2129
+
+set +e
+
+if [ -z "$CLOUDSMITH_SERVICE_ACCOUNT" ] || [ -z "$CLOUDSMITH_OIDC_TOKEN" ]
+then
+  echo "Unable to find an OIDC token to use. Please ensure the cloudsmith-oidc/authenticate_with_oidc command has been run before this command."
+  exit 1
+fi
+
+echo "export CLOUDSMITH_POETRY_USERNAME=\"$CLOUDSMITH_SERVICE_ACCOUNT\"" >> "$BASH_ENV"
+echo "export CLOUDSMITH_POETRY_PASSWORD=\"$CLOUDSMITH_OIDC_TOKEN\"" >> "$BASH_ENV"
+
+echo "The following environment variables have been exported. Note, CLOUDSMITH_POETRY_PASSWORD has been masked below."
+echo ""
+echo "CLOUDSMITH_POETRY_USERNAME=$CLOUDSMITH_POETRY_USERNAME"
+echo "CLOUDSMITH_POETRY_PASSWORD=********"
